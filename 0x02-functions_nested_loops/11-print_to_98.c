d0bb5e585559d6384b208c306b2c1b4f8a634fcd
Lekoba838 <lekobalucia755@gmail.com>
2022-09-14 03:45:07 -0700

    11-print_to_98.c

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * add - Adds two intergers
+ * @num1:The first interger to be added
+ * @num2: second interger to be added
+ *
+ * Return: The results of the addition
+ */
+int add(int num1, int num2)
+{
+	return (num1 + num2);
+}
