aa87e33529cbc40b8a64591af9632118f83665fb
Lekoba838 <lekobalucia755@gmail.com>
2022-09-15 13:30:08 -0700

    0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -2,15 +2,17 @@
 
 /**
  * _isupper - check if a letter is upper
- * @x: Rhe number to be checked
- *
- * Return: 1 for upper letter or 0 for any else
+ * @c: @c: print int
+ * Return: Always 0 (Success)
  */
- int _isupper(int x)
+ int _isupper(int c)
 {
-	if (x >= 65 && x <= 90)
+	if (c >= 'A' && c <= 'Z')
 	{
 		return (1);
 	}
+	else
+	{
 	return (0);
+	}
 }
