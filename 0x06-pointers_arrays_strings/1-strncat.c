f557439ae44884f63ed2150477bc678437b8c8a1
Lekoba838 <lekobalucia755@gmail.com>
2022-09-21 15:23:11 -0700

    1-strncat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -5,18 +5,22 @@
  * @src: string to be completed at the end of dest
  * @n: integer parameter to compare index to
  *
- * Return: returns new concatenated string
+ * Return: @dest
  */
 
 char *_strncat(char *dest, char *src, int n)
 {
-	int index = 0, dest_len = 0;
+	int c, i;
 
-	while (dest[index++])
-		dest_len++;
+	c = 0;
 
-	for (index = 0; src[index] && index < n; index++)
-		dest[dest_len++] = src[index];
+	while (dest[c])
+		c++;
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
+		dest[c + i] = src[i];
+
+	dest[c + i] = '\0';
 
 	return (dest);
 }
