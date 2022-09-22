931a7087ec6168cb4eaae3b7204971d37cb1205c
Lekoba838 <lekobalucia755@gmail.com>
2022-09-22 08:17:00 -0700

    3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -9,22 +9,29 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	int comp = 0, i = 0;
+	int i = 0, diff = 0;
 
-	while (s1[i] != '\0' && s2[i] != '\0')
+	while (1)
 	{
-		if (s1[i] != s2[i])
+		if (s1[i] == '\0' && s2[i] == '\0')
+			break;
+		else if (s1[i] == '\0')
 		{
-			comp = 1;
+			diff = s2[i];
 			break;
 		}
-		i++;
+		else if (s2[i] == '\0')
+		{
+			diff = s1[i];
+			break;
+		}
+		else if (s1[i] != s2[i])
+		{
+			diff = s1[i] - s2[i];
+			break;
+		}
+		else
+			i++;
 	}
-	if (comp == 0)
-
-		return (0);
-
-	else
-
-		return (1);
+	return (diff);
 }
