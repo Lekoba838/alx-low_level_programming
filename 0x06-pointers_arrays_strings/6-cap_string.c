7a84c6c483c947c2959db966e335e171932956fa
Lekoba838 <lekobalucia755@gmail.com>
2022-09-25 14:35:55 -0700

    104-print_buffer.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -26,7 +26,7 @@ char *cap_string(char *str)
 
 	while (str[++index])
 	{
-		while (!(str[index] >= 'a') && (str[index] <= 'z'))
+		while (!(str[index] >= 'a' && str[index] <= 'z'))
 			index++;
 
 		if (str[index - 1] == ' ' ||
@@ -41,8 +41,11 @@ char *cap_string(char *str)
 				str[index - 1] == '(' ||
 				str[index - 1] == ')' ||
 				str[index - 1] == '{' ||
-				str[index - 1] == '}')
+				str[index - 1] == '}' ||
+				index == 0)
 			str[index] -= 32;
+
+		index++;
 	}
 	return (str);
 }
