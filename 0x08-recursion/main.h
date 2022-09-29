097ef943a2570883e39e5073cbd35da62da2966c
Lekoba838 <lekobalucia755@gmail.com>
2022-09-29 12:46:54 -0700

    100-is_palindrome.c

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -13,4 +13,7 @@ int _sqrt_recursion(int n);
 int _sqrt(int n, int x);
 int is_prime_number(int n);
 int is_prime(int n, int start);
+int is_palindrome(char *s);
+int _strlen_recursion(char *s);
+void check(char *s, int start, int end, int *flag);
 #endif /* MAIN_H */
