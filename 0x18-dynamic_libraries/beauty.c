5070f430187059ea6c4560e4219dbe03bac4c0ab
root <root@LUCIA.localdomain>
2022-12-13 00:35:33 +0200

    task3

---
+++ 0x18-dynamic_libraries/beauty.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+
+/* add - add two integers
+ * @a: input integer
+ * @b: input integer
+ * Return: the sum
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/* sub - sub two integers
+ * @a: input integer
+ * @b: input integer
+ * Return: the sub
+ */
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/* mul - mul two integers
+ * @a: input integer
+ * @b: input integer
+ * Return: the mul
+*/
+int mul(int a, int b)
+{
+        return (a * b);
+}
+
+/* div - div two integers
+ * @a: input integer
+ * @b: input integer
+ * Return: the div
+*/
+int div(int a, int b)
+{
+        return (a / b);
+}
+
+/* mod - mod two integers
+ * @a: input integer
+ * @b: input integer
+ * Return: the mod
+*/
+int mod(int a, int b)
+{
+        return (a % b);
+}
